f7b8736594f90631d63815736240edf40da17e91
twotabsofacid <seanmichaelscanlan@gmail.com>
2022-10-01 21:46:10 -0400

    Adding in squiggle idk

---
+++ sketches/squiggle/script.js
@@ -0,0 +1,46 @@
+let pointsArr = [];
+
+function setup() {
+  let w = window.innerWidth;
+  let h = window.innerHeight;
+  createCanvas(w, h, SVG);
+  noFill();
+  angleMode(DEGREES);
+  runDraw();
+}
+
+function runDraw() {
+  background(255);
+  stroke(0);
+  drawSquiggle(50, 100);
+}
+
+function drawSquiggle(totalPoints, r) {
+  push();
+  translate(width / 2, height / 2);
+  beginShape();
+  for (let i = 0; i < totalPoints; i++) {
+    let deg = map(i, 0, totalPoints - 1, 0, 360);
+    let xPos = r * cos(deg);
+    console.log((noise(sin(i)) - 0.5) * 500);
+    let yPos = r * sin(deg);
+    let mappedI = abs(map(i, 0, totalPoints - 1, -totalPoints, totalPoints));
+    let noiseX = (noise(sin(mappedI)) - 0.5) * 223;
+    let noiseY = (noise(cos(mappedI)) - 0.5) * 287;
+    vertex(xPos + noiseX, yPos + noiseY);
+  }
+  endShape();
+  pop();
+}
+
+function keyReleased(e) {
+  console.log(e);
+  if (e.code === 'Space') {
+    save('squiggle.svg');
+  }
+}
+
+function mouseClicked() {
+  noiseSeed(millis());
+  runDraw();
+}
